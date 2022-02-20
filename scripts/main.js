737769b8515a53bd54aca485a2abc2cd4b4408e6
Yasser-Slaymen <yaserslyman0@gmail.com>
2022-02-20 20:22:26 +0100

    js updating

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
-console.log('halllo')
+// nog niet klaar
+const button = document.querySelector('a');
+// button.forEach()
+button.addEventListener('click', function() {
+  button.style.background = 'red';
+  
+});
+// const btn = document.querySelector('.front')
+// btn.addEventListener('click',function(m){
+//     btn.getElementsByClassName.color = "red";
+// });
